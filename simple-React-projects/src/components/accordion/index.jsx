ff9f37394521bf924a4fc71f69cbb93d20a110f5
艾因 <lerrannpasar@gmail.com>
2025-02-22 12:11:46 -0500

    accordion enable both single selection and multi selection

--- simple-React-projects/src/components/accordion/index.jsx
+++ simple-React-projects/src/components/accordion/index.jsx
@@ -4,13 +4,42 @@ import './styles_2.css';
 
 function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [selectedArr, setSelectedArr] = useState([]);
+  const [enableMultiSel, setEnableMultiSel] = useState(false);
 
   function handelClickItem(itemId) {
-    setSelected(selected === itemId ? null : itemId);
+    !enableMultiSel
+      ? setSelected(selected === itemId ? null : itemId)
+      : setSelectedArr(
+          selectedArr.includes(itemId)
+            ? selectedArr.filter((id) => id !== itemId)
+            : selectedArr.concat(itemId)
+        );
+  }
+
+  function handleClickBtn() {
+    // enableMulti --> disEnable, eg. multi selection --> single selection
+    if (enableMultiSel) {
+      setSelected(selectedArr.pop());
+      setSelectedArr([]); // clear multi selection data
+    } else {
+      setSelectedArr((s) => s.concat(selected));
+      setSelected(null);
+    }
+
+    setEnableMultiSel((e) => !e);
   }
 
   return (
     <div className="wrapper">
+      <button
+        onClick={handleClickBtn}
+        style={{ backgroundColor: !enableMultiSel ? '#614101' : '#943b0e' }}
+      >
+        {!enableMultiSel
+          ? ' Enable Multi Selection'
+          : 'Disable Multi Selection'}
+      </button>
       <div className="accordion">
         {data && data.length > 0
           ? data.map((item, id) => (
@@ -19,7 +48,8 @@ function Accordion() {
                   <h3>{item.question}</h3>
                   <span>+</span>
                 </div>
-                {selected === id && (
+                {/* {selected === id && ( */}
+                {(selected === id || selectedArr.includes(id)) && (
                   <div className="content">
                     <p>{item.answer}</p>
                   </div>
