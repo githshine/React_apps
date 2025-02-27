0548bb7543a7ac30a506bccdb3787eb053148601
艾因 <lerrannpasar@gmail.com>
2025-02-27 10:08:34 -0500

    logical or operator && use state to define dynamic className, then define different UI

--- simple-React-projects/src/App.jsx
+++ simple-React-projects/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div id="app">
       {/* <Accordion /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating />
     </div>
   );
 }
---
+++ simple-React-projects/src/components/star-rating/index.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+import './styles.css';
+
+function StarRating({ noOfStars = 5 }) {
+  // console.log(Array(5));
+  // console.log([...Array(5)]);
+
+  // const [endIndex, setEndIndex] = useState(-1);
+  const [endIndex, setEndIndex] = useState(0);
+
+  // 1) HOW TO CHOOSE A STATE: when it changes, something UI also changes,
+  // then you need to recored this state!!
+
+  // 2) HOW TO SET INITIAL STATE
+  // if initState = -1, then when calculating index <= (hovingIndex || endIndex)
+  // when hover the first element, hovingIndex will be considered as false(0),
+  // therefore will use endIndex's value ==> cause errors
+  // const [hovingIndex, setHovingIndex] = useState(-1);
+  const [hovingIndex, setHovingIndex] = useState(0);
+
+  function handleOnClick(index) {
+    setEndIndex(index);
+  }
+  function handleOnMouseEnter(index) {
+    setHovingIndex(index);
+  }
+  function handleOnMouseLeave() {
+    setHovingIndex(endIndex);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index++;
+
+        return (
+          <FaStar
+            // className="star"
+            key={index}
+            className={
+              index <= (hovingIndex || endIndex) ? 'active' : 'nonActive'
+            }
+            onClick={() => handleOnClick(index)}
+            onMouseMove={() => handleOnMouseEnter(index)}
+            onMouseLeave={() => handleOnMouseLeave()}
+            size={40}
+          ></FaStar>
+        );
+      })}
+    </div>
+  );
+}
+
+export default StarRating;
