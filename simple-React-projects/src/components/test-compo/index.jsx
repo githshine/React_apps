f1e4a7c0ec2151da3fba4b926ceb6c5acf9a5f3e
艾因 <lerrannpasar@gmail.com>
2025-03-16 21:16:16 -0400

    custom hooks usage

--- simple-React-projects/src/components/test-compo/index.jsx
+++ simple-React-projects/src/components/test-compo/index.jsx
@@ -1,21 +1,38 @@
-let guest = 0;
+import { useState, useEffect } from 'react';
 
-function Cup() {
-  // Bad: changing a preexisting variable!
-  guest = guest + 1;
-  return <h2>Tea cup for guest #{guest}</h2>;
-}
+export default function ShippingForm({ country }) {
+  const [cities, setCities] = useState(null);
+  // This Effect fetches cities for a country
+  useEffect(() => {
+    let ignore = false;
+    fetch(`/api/cities?country=${country}`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) {
+          setCities(json);
+        }
+      });
+    return () => {
+      ignore = true;
+    };
+  }, [country]);
 
-// Output:  why? Because in strict mode, React render component twice during development.
-// Tea cup for guest #2
-// Tea cup for guest #4
-// Tea cup for guest #6
-export default function TeaSet() {
-  return (
-    <>
-      <Cup />
-      <Cup />
-      <Cup />
-    </>
-  );
+  const [city, setCity] = useState(null);
+  const [areas, setAreas] = useState(null);
+  // This Effect fetches areas for the selected city
+  useEffect(() => {
+    if (city) {
+      let ignore = false;
+      fetch(`/api/areas?city=${city}`)
+        .then((response) => response.json())
+        .then((json) => {
+          if (!ignore) {
+            setAreas(json);
+          }
+        });
+      return () => {
+        ignore = true;
+      };
+    }
+  }, [city]);
 }
